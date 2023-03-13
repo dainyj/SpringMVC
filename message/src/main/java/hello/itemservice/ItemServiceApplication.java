b548b22c33fabd9ada826965a1460bcb07b9c536
dainyj <jdatwo@naver.com>
2023-03-13 11:31:59 +0900

    MVC - 메시지, 국제화

---
+++ message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,26 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class, args);
+    }
+
+
+	//직접 등록하는 경우
+//    @Bean
+//    public MessageSource messageSource() {
+//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//        messageSource.setBasenames("messages", "errors");
+//        messageSource.setDefaultEncoding("utf-8");
+//        return messageSource;
+//    }
+
+}
